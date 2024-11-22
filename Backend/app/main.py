e67f48d181cf23485384599032269f6b4740f6ae
levi-discente <lrsaq@discente.ifpe.edu.br>
2024-11-22 00:37:31 -0300

    add: cors

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,13 +1,23 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routes import router
 from app.database import init_db
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield init_db()
 
 
-@app.on_event("startup")
-def on_startup():
-    init_db()
+app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
